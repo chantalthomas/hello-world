bbd732c8bb2d02bd890b2acb9ec88cdaf354370d
Chantal Thomas <thomas.chantala@gmail.com>
2018-09-14 08:18:44 -0400

    updated content on main page

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	<link href="css/main.css" rel="stylesheet" type="text/css" >
 </head>
 <body>
-	<h1>Hello People</h1>
+	<h1>Hello World</h1>
+	<h2>Frenchie</h2>
 	<img src='images/frenchie.jpg'
 </body>	
 </html>
